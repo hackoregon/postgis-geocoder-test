c6f6c79d5c16c0fa8bb66bd50bfb0600a1f405de
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-12-20 02:50:14 -0800

    get rid of license noise

--- Linux/1configure-postgresql.bash
+++ Linux/1configure-postgresql.bash
@@ -1,13 +1,4 @@
 #! /bin/bash
-#
-# Copyright (C) 2014 by M. Edward (Ed) Borasky
-#
-# This program is licensed to you under the terms of version 3 of the
-# GNU Affero General Public License. This program is distributed WITHOUT
-# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
-# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
-# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
-#
 
 sudo postgresql-setup initdb # fails harmlessly if data directory isn't empty
 sudo systemctl enable postgresql # start the server on reboot
