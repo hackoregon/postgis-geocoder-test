2fd234e2c8586f85345a3a40a83f29524ba68d5f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-12-20 18:02:37 -0800

    do everything into "postgres" database

--- Linux/2make-geocoder-download-scripts.bash
+++ Linux/2make-geocoder-download-scripts.bash
@@ -6,20 +6,17 @@ sudo mkdir -p /gisdata/temp
 sudo chown -R ${USER}:${USER} /gisdata
 
 # execute script builder
-psql -f make-tiger-scripts.psql geocoder znmeb
+psql -h localhost -f make-tiger-scripts.psql postgres postgres
 chmod +x /gisdata/*.bash
 
-# fix path
+# edit the scripts
 for i in '/gisdata/state-county.bash' '/gisdata/oregon.bash'
 do
   sed -i 's;export PGBIN=.*$;export PGBIN=/usr/bin;' ${i}
-done
-
-# comment out 'localhost' setting so we can use UNIX sockets
-for i in '/gisdata/state-county.bash' '/gisdata/oregon.bash'
-do
+  sed -i 's;export PGDATABASE=.*$;export PGDATABASE=postgres;' ${i}
   sed -i 's;export PGHOST=localhost;#export PGHOST=localhost;' ${i}
 done
+
 
 # change owner to 'postgres' - the generated scripts have to run as 'postgres'
 sudo chown -R postgres:postgres /gisdata
