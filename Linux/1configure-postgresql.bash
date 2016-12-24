4ea8d6980d7c698eb92f62696ffef2c961ee17c9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-12-24 15:47:09 -0800

    use superuser powers to create extensions

--- Linux/1configure-postgresql.bash
+++ Linux/1configure-postgresql.bash
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-sudo postgresql-setup initdb # fails harmlessly if data directory isn't empty
+sudo postgresql-setup --initdb --unit postgresql
+# fails harmlessly if data directory isn't empty
+
 sudo systemctl enable postgresql # start the server on reboot
 sudo systemctl start postgresql # start the server now
 
@@ -9,5 +11,4 @@ echo "Create a password for 'postgres', the PostgreSQL superuser"
 sudo su - postgres -c "psql -c '\password postgres'"
 
 # install the extensions - will ERROR harmlessly if they're already there
-export HERE=`pwd`
-sudo su - postgres -c "psql -f ${HERE}/extensions.sql"
+psql -d postgres -f extensions.sql
