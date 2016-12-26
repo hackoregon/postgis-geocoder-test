d50cb3a0245961db6e526685d62df4153f056f60
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-12-25 21:54:37 -0800

    leave the database name as default "geocoder"

--- Linux/2make-geocoder-download-scripts.bash
+++ Linux/2make-geocoder-download-scripts.bash
@@ -6,14 +6,13 @@ sudo mkdir -p /gisdata/temp
 sudo chown -R ${USER}:${USER} /gisdata
 
 # execute script builder
-psql -h localhost -f make-tiger-scripts.psql postgres postgres
+psql -d geocoder -f make-tiger-scripts.psql
 chmod +x /gisdata/*.bash
 
 # edit the scripts
 for i in '/gisdata/nation.bash' '/gisdata/oregon.bash'
 do
   sed -i 's;export PGBIN=.*$;export PGBIN=/usr/bin;' ${i}
-  sed -i 's;export PGDATABASE=.*$;export PGDATABASE=postgres;' ${i}
   sed -i 's;export PGPASSWORD;#export PGPASSWORD;' ${i}
   sed -i 's;export PGHOST;#export PGHOST;' ${i}
 done
