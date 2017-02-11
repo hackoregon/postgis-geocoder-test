0bae516e2fabcbbfe6d5512e4293d8fec453ffaf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-02-11 15:35:05 -0800

    Create a PostGIS template

--- Docker/create-geocoder-database.bash
+++ Docker/create-geocoder-database.bash
@@ -1,10 +1,11 @@
 
 #! /bin/bash
 
-echo "Creating a geocoder database"
+echo "Creating a PostGIS template and geocoder database"
 echo "If you've already created the geocoder database you can ignore the error messages."
-createdb -O postgres geocoder
-psql -d geocoder -f extensions.sql
+createdb -O postgres postgis_template
+psql -d postgis_template -f extensions.sql
+createdb -O postgres geocoder -T postgis_template
 
 # execute script builder
 psql -d geocoder -f make-tiger-scripts.psql
