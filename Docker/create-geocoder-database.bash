d6ca2e9ed9321e13cd5173b197fddf25ddb20756
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-02-11 16:43:40 -0800

    no template any more

--- Docker/create-geocoder-database.bash
+++ Docker/create-geocoder-database.bash
@@ -3,7 +3,7 @@
 
 echo "Creating a geocoder database"
 echo "If you've already created the geocoder database you can ignore the error messages."
-createdb -O postgres -T postgis_template geocoder
+createdb -O postgres geocoder
 psql -d geocoder -f extensions.sql
 
 # execute script builder
