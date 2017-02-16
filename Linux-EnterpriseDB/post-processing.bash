c78d08fd4f83e549b389d05d152524995b715c0a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-02-15 16:48:04 -0800

    smoke test done

--- Linux-EnterpriseDB/post-processing.bash
+++ Linux-EnterpriseDB/post-processing.bash
@@ -3,5 +3,10 @@
 source env-enterprisedb
 sudo chown -R ${USER}:${USER} /gisdata
 
+echo "Installing any missing indexes"
 psql -d geocoder -c "SELECT install_missing_indexes();"
+echo "Creating a dump of the geocoder database"
 pg_dump -Fc geocoder > /gisdata/geocoder.pgdump
+
+echo "Testing the geocoder - compare lon and lat with Google Maps"
+psql -d geocoder -f test-geocoder.sql
