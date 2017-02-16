e85c3bf4251d262723f37177ce232f84a8ed36e3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-02-15 16:49:10 -0800

    not using this

--- gisdata-image/hack_oregon_headquarters.sql
+++
@@ -1,4 +0,0 @@
-SELECT g.rating, ST_X(g.geomout) As lon, ST_Y(g.geomout) As lat,
-    (addy).address As stno, (addy).streetname As street,
-    (addy).streettypeabbrev As styp, (addy).location As city, (addy).stateabbrev As st,(addy).zip
-    FROM geocode('317 NW Glisan St Portland, OR 97209') As g;
--- gisdata-image/startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# create geocoder database if it doesn't exist
-if [ -e "/gisdata/geocoder.pgdump" ]
-then
-  pg_restore /gisdata/geocoder.pgdump
-else
-  /gisdata/create-geocoder-database.bash
-fi
