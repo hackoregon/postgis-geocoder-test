f6dc72350b6869a98472a6bd8961c80a312c11f8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-02-15 20:17:19 -0800

    Download geocoder.pgdump from GitHub

--- postgis/create-database.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-createdb -O postgres geocoder
-psql -d geocoder < create-extensions.sql
-psql -c "SELECT loader_generate_nation_script(’sh’);" -d geocoder -tA > /gisdata/nation.sh
-psql -c "SELECT loader_generate_script(ARRAY[’OR’], 'sh')" -d geocoder -tA > /gisdata/oregon.sh
