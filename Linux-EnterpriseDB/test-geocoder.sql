a9a3c250381002e9e247c7113f5ad5d145ae2b45
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-02-16 01:07:45 -0800

    add intersection to test case!

--- Linux-EnterpriseDB/test-geocoder.sql
+++ Linux-EnterpriseDB/test-geocoder.sql
@@ -7,3 +7,11 @@ FROM
   geocode(
     '317 NW Glisan St, Portland, OR 97209'
   ) As g;
+SELECT
+  pprint_addy(addy),
+  st_astext(geomout),
+  rating
+FROM
+  geocode_intersection(
+    'Glisan', '3rd', 'OR', 'Portland'
+  );
