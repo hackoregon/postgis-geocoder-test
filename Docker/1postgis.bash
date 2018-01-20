f38b4506774665793833aa2222adccb7fa011af9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-01-19 21:49:01 -0800

    renaming things

---
+++ Docker/1postgis.bash
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# create the empty PostGIS image and start it - resulting image is 458 MB as of 2018-01-19
+echo "Building the Docker PostGIS image"
+docker-compose -f postgis_image.yml up --build -d
