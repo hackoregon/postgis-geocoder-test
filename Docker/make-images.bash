9aa8a811aa996dd68f838505a066b1e9ae8cf31a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-01-20 09:23:52 -0800

    ready for smoke test

--- Docker/make-images.bash
+++ Docker/make-images.bash
@@ -1,29 +1,8 @@
 #! /bin/bash
 
-echo "Bringing up the 'postgis' service."
-docker-compose -f postgis.yml up --build -d
+echo "Building the vanilla 'postgis' image."
+docker-compose -f postgis.yml build
 
-# build the database and dump it if needed - takes a while!
-if [ ! -e /data/gisdata/geocoder.pgdump ]
-then
-  echo "Creating the 'gisdata' host volume."
-  sudo rm -fr /data/gisdata
-  sudo mkdir -p /data/gisdata
-  sudo chown 999:999 /data/gisdata
-  echo "Sleeping 30 seconds to wait for PostGIS to start."
-  echo "The database load / dump creation will take off after that and will run for some time."
-  sleep 30
-  docker exec docker_postgis_1 /start-creation.bash
-else
-  echo "The database dump file already exists."
-fi
-
-# we don't need the 'postgis' service any more
-echo "Bringing down the 'postgis' service."
-docker-compose -f postgis.yml down
-
-# copy dump here
-cp /data/gisdata/geocoder.pgdump .
-
-echo "Bringing up the 'postgis-geocoder' service."
-docker-compose -f postgis-geocoder.yml up --build
+echo "Building the 'postgis-geocoder' image."
+echo "This will also build the geocoder database onto a persistent host volume."
+docker-compose -f postgis-geocoder.yml build
